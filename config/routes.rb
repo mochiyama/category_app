0f281553f2b867722b2a94ba46b92fe9ee292bc2
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-03-09 23:15:37 +0900

    routes.rb追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'categories/new'
   root 'categories#new'
- resources :categories, only:[:new]
+  resources :categories, only:[:new]
+  get '/category/:id', to: 'categories#search'
 end
