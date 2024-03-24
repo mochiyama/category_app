d59c1ef920e0694741ff8cebab333da6fab38243
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-03-24 11:57:52 +0900

    親カテゴリ表示~まで修正

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -7,27 +7,9 @@ window.addEventListener('turbo:load', function () {
 
   }
 
-  const XHR = new XMLHttpRequest();
-  const categoryXHR = (id) => {
-    XHR.open("GET", `/category/${id}`, true);
-    XHR.responseType = "json";
-    XHR.send();
-  }
-
-  const getChildCategoryData = () => {
-    const parentValue = parentCategory.value
-    categoryXHR(parentValue)
-
-    XHR.onload = () => {
-      const items = XHR.response.item;
-    }
-  }
-
-
 
   parentCategory.addEventListener('change', function () {
     selectChildElement('child-select-wrap')
-    getChildCategoryData()
   })
 
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'categories#new'
-  resources :categories, only:[:new]
-  get '/category/:id', to: 'categories#search'
+
+
 end
