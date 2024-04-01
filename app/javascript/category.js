e6e315426eda6fe9fa5f6e97d1c03a407bace73f
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-04-02 00:31:41 +0900

    category.js編集

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,12 +1,10 @@
 window.addEventListener('turbo:load', function () {
 
   const parentCategory = document.getElementById('parent-category')
-  const selectWrap = document.getElementById('select-wrap')
 
   const selectChildElement = (selectForm) => {
 
   }
-
 
   parentCategory.addEventListener('change', function () {
     selectChildElement('child-select-wrap')
