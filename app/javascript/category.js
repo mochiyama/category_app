4c39e95e84b85fa712334c00059252e201525266
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-04-11 02:02:38 +0900

    子カテゴリーのプルダウン表示1st step

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,7 +1,7 @@
 window.addEventListener('turbo:load', function () {
 
   const parentCategory = document.getElementById('parent-category')
-
+  const selectWrap = document.getElementById('select-wrap')
   const selectChildElement = (selectForm) => {
 
   }
